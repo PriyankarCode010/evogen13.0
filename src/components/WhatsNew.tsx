144a0bc8b4948f82493b028693595a2e0c86fe38
Priyankar_010_ <109033813+PriyankarCode010@users.noreply.github.com>
2024-05-28 21:58:17 +0530

    1st

--- src/components/WhatsNew.tsx
+++ src/components/WhatsNew.tsx
@@ -22,7 +22,7 @@ const WhatsNew = () => {
     <div className='flex flex-col my-10'>
       <h1 className='text-red-600 text-4xl md:text-5xl lg:text-6xl xl:text-7xl font-extrabold px-5'>What&apos;s New</h1>
       <div className='flex flex-row justify-center items-center'>
-        <div className='left p-3'>
+        <div className='left p-2'>
           <button 
             className="p-2 rounded-full bg-red-700 text-white hover:bg-red-600"
             onClick={scrollLeft}
@@ -33,14 +33,14 @@ const WhatsNew = () => {
         <div className='overflow-y-hidden lg:overflow-x-hidden' ref={scrollContainerRef}>
           <div className='flex flex-row py-7'>
             {eventsData.map((event, index) => (
-              <div key={index} className='flex-1 h-auto min-w-[274px] md:min-w-[400px] border-l-2 border-red-600'>
+              <div key={index} className='flex-1 h-auto min-w-[274px] md:min-w-[420px] border-l-2 border-red-600'>
                 <h1 className='text-3xl px-2'>{event.name}</h1>
                 <p className='p-4'>{event.description}</p>
               </div>
             ))}
           </div>
         </div>
-        <div className='right p-3'>
+        <div className='right p-2'>
           <button 
             className="p-2 rounded-full bg-red-700 text-white hover:bg-red-600"
             onClick={scrollRight}
--- src/data/eventsData.js
+++ src/data/eventsData.js
@@ -1,232 +1,258 @@
 const eventsData = [
-    {
-      id: "coding",
-      name: "Iron-Script",
-      tagline: "Iron out your bugs, suit up your scripts.",
-      logo: "./Images/winners.webp",
-      description:
-        "Iron Script is the ultimate coding event for students looking to unlock their true potential and take their coding skills to the next level. With a diverse range of fun and challenging coding problems to solve, participants will have the opportunity to push their limits and expand their knowledge.",
-      rules: [
-        "Number of participants is 03, one from each sem is compulsory.",
-        "Skills required - Knowledge of C Language, Python, Java, Problem Solving, DSA, HTML/CSS, JavaScript.",
-        "Requirements - 1 Laptop, 1 Mobile.",
-        "Use of internet is prohibited until told so!",
-      ],
-      heads: [
-        {
-          name: "Prathamesh Sirdesai",
-          phno: 7022112496,
-          photo: "/Photos/sirdesai.jpg",
-        },
-        {
-          name: "Sugandh Revankar",
-          phno: 8277067587,
-          photo: "/Photos/sugandh.jpg",
-        },
-      ],
-      faculty: [{
-        name: "Prof. Supriya Balekundri",
-        phno: 8073579756,
-        photo: "/Photos/supriya.jpg",
-      },]
-    },
-    {
-      id: "designing",
-      name: "Thunder Canvas",
-      tagline: "Unleashing creative storms.",
-      logo: "./Images/winners.webp",
-      description:
-        "Unleash your creativity and join us for a designing event that will inspire and challenge you. This event is the perfect opportunity to explore new techniques, connect with like-minded individuals. Don't miss this chance to push your boundaries and take your skills to the next level.",
-      rules: [
-        "Number of participants is 03, one from each sem is compulsory.",
-        "Skills required - Basic Graphic designing knowledge,Time mangement skills, Arts.",
-        "Requirements - 2 Laptop, Pendrive, Headphones (Mandatory).",
-      ],
-      heads: [
-        {
-          name: "Prathamesh Patil",
-          phno: 9113061708,
-          photo: "/Photos/prince.jpg",
-        },
-        {
-          name: "Samarth Muthgekar",
-          phno: 8050496719,
-          photo: "/Photos/samarth.jpg",
-        },
-      ],
-      faculty: [{
-        name: "Prof. Rucha Bapat",
-        phno: 9900619694,
-        photo: "/Photos/rucha.jpg",
-      },]
-    },
-    {
-      id: "communication",
-      name: "Cosmic Convo",
-      tagline: "Unraveling glorius truths.",
-      logo: "./Images/winners.webp",
-      description:
-        "Presence of mind, courage to speak, and the quality of communication determine the fruits you reap. Gladiatorial combat and understanding what isn't heard are all that you need to stand out among the intellects.",
-      rules: [
-        "Number of participants is 03, one from each sem is compulsory.",
-        "Skills required - Wordplay, Roleplay, Debating, Spoken English, Good Vocabulary, Presentation",
-        "Requirements - 1 Laptop, Pendrive, Smartphone.",
-      ],
-      heads: [
-        {
-          name: "Ritesh Navalgund",
-          phno: 7057561603,
-          photo: "/Photos/ritesh.jpg",
-        },
-        {
-          name: "Vaishnavi Phulwale",
-          phno: 7337741473,
-          photo: "/Photos/vaishnavi_phulwale.jpg",
-        },
-      ],
-      faculty: [{
-        name: "Dr. Chidambar Inamdar",
-        phno: 9620900846,
-        photo: "/Photos/chidambar.jpg",
+  {
+    id: "coding",
+    name: "Paradox",
+    tagline: "The end is the beginning and the beginning is the end.",
+    logo: "./Images/winners.webp",
+    description:
+      "Paradox is the ultimate coding event for students aiming to unlock their true potential and elevate their coding skills. With a diverse range of fun and challenging coding problems to solve, participants will have the opportunity to push their limits and expand their knowledge.",
+    rules: [
+      "Number of participants is 02.",
+      "Skills required - problem solving, decision making, C, Java, Python, HTML CSS, JavaScript",
+      "Requirements - 2 laptops, mobile, earphones (wired)",
+      "Use of internet is prohibited until told so!",
+    ],
+    heads: [
+      {
+        name: "Prathamesh Sirdesai",
+        phno: 7022112496,
+        photo: "/Photos/sirdesai.jpg",
       },
       {
-        name: "Prof. Karuna Deshpande",
-        phno: 8747015516,
-        photo: "/Photos/karuna.jpg",
-      },]
+        name: "Priyankar Haldar",
+        phno: 8277067587,
+        photo: "/Photos/priyankar.jpg",
+      },
+    ],
+    faculty: [{
+      name: "Prof.Vaibhav Ambekar",
+      phno: 8073579756,
+      photo: "/Photos/Vaibhav.jpg",
+    },{
+      name: "Prof.Shruti Kanchungar",
+      phno: 8073579756,
+      photo: "/Photos/Shruti.jpg",
+    },{
+      name: "Mrs. Radhika",
+      phno: 8073579756,
+      photo: "/Photos/radhika.jpg",
     },
-    {
-      id: "cyber-security",
-      name: "Crypto Verse",
-      tagline: "Spidey's Web Security Hub: Where Marvel meets the Cyber Club!",
-      logo: "./Images/winners.webp",
-      description:
-        "Cryptoverse is basically a cyber event that describes the theft done on the used data. Cybersecurity is the practice of protecting critical systems and sensitive information from digital attacks. Cybersecurity measures are designed to combat threats.",
-      rules: [
-        "Number of participants is 03, one from each sem is compulsory.",
-        "Skills required - Basic Linux commands, services & basic knowledge about web.",
-        "Requirements - 1 Laptop.",
-      ],
-      heads: [
-        {
-          name: "Amar Badiger",
-          phno: 8861142545,
-          photo: "/Photos/amar.jpg",
-        },
-        {
-          name: "Sushma Yamakanamaradi",
-          phno: 7892288512,
-          photo: "/Photos/sushma.jpg",
-        },
-      ],
-      faculty: [{
-        name: "Prof. Govind Huligol",
-        phno: 9986296434,
-        photo: "/Photos/govind.jpg",
-      },]
-    },
-    {
-      id: "data-analytics",
-      name: "Analytix Nexus",
-      tagline: "Connecting Insights",
-      logo: "./Images/winners.webp",
-      description:
-        "Dive into the world of data analytics, where numbers tell stories, and insights are the treasures waiting to be discovered. Here, every row and column is a potential adventure. You will dance with algorithms, juggle statistical models, and ride the roller coaster of trends. Get ready to wear your capes and step into the realm of data analytics.",
-      rules: [
-        "Number of participants is 03, one from each sem is compulsory.",
-        "Skills required - Oracle, MongoDB, Jupyter, WinRAR, Excel, R Language, Pandas, Basic knowledge of Databases.",
-        "Requirements - Laptop, Pendrive (Mandatory).",
-      ],
-      heads: [
-        {
-          name: "Aparna Shekar",
-          phno: 8746970092,
-          photo: "/Photos/aparna.jpg",
-        },
-        {
-          name: "Vaishnavi Bongale",
-          phno: 7760882407,
-          photo: "/Photos/vaishnavi_bong.jpg",
-        },
-      ],
-      faculty: [{
-        name: "Prof. Shruti Kulkarni",
-        phno: 8792242249,
-        photo: "/Photos/shruti.jpg",
+
+  ]
+  },
+  {
+    id: "technocrats",
+    name: "The Extraordinary Academy",
+    tagline: "Ordinary is Overrated.",
+    logo: "./Images/winners.webp",
+    description:
+    "Embark on a journey of intellectual prowess and finesse where extraordinary people meet and unleash their pure potential with all skills required to be a master of all traits. Participants demonstrate their mastery across a spectrum of disciplines, from the cerebral realms of logic and problem-solving to the artistry of performance and expression many much more.",
+    rules: [
+      "Number of participants is 01.",
+      "Skills required - Creativity, Logo and Poster Designing, Basics of HTML/CSS, Presentation, Basic Linux Knowledge.",
+      "Requirements - Laptop (Photoshop, VS Code Pre Installed), Smartphone, Pen drive.",
+    ],
+    heads: [
+      {
+        name: "Ritesh Navalgund",
+        phno: 9113061708,
+        photo: "/Photos/ritesh.jpg",
       },
       {
-        name: "Prof. Jyoti Patil",
-        phno: 8884988033,
-        photo: "/Photos/jyoti.jpeg",
-      },]
+        name: "Akshata Patil",
+        phno: 8050496719,
+        photo: "/Photos/akshata.jpg",
+      },
+    ],
+    faculty: [{
+      name: "Prof.Supriya Balekundri",
+      phno: 9900619694,
+      photo: "/Photos/supriya.jpg",
+    },{
+      name: "Prof. Vaishali Kulkarni",
+      phno: 9900619694,
+      photo: "/Photos/vaishali.jpg",
+    },{
+      name: "Ms. Aniketa",
+      phno: 9900619694,
+      photo: "/Photos/aniketa.jpg",
+    },
+  ]
+  },
+  {
+    id: "communication",
+    name: "Cerebro Voice",
+    tagline: "Where communication meets mystery.",
+    logo: "./Images/winners.webp",
+    description:
+    "Dive into 'Upside Voice' where communication becomes an adventure. Engage in themed challenges blending mystery and creativity, from decoding messages to navigating twists in speeches.",
+    rules: [
+      "Number of participants is 01.",
+      "Skills required - wordplay, debating, spoken english, good vocabulary, presentation, roleplay.",
+      "Requirements - 01 laptop, pendrive smartphones.",
+    ],
+    heads: [
+      {
+        name: "Aparna Shekhar",
+        phno: 7057561603,
+        photo: "/Photos/aparna.jpg",
+      },
+      {
+        name: "Aditya Kalagounda",
+        phno: 7337741473,
+        photo: "/Photos/aditya.jpg",
+      },
+    ],
+    faculty: [{
+      name: "Dr. Chidambar Inamdar",
+      phno: 9620900846,
+      photo: "/Photos/chidambar.jpg",
     },
     {
-      id: "gaming",
-      name: "Hysteria",
-      tagline: "Battles await, Heroes rise.",
-      logo: "./Images/winners.webp",
-      description:
-        "Upgrade your reflexes and explore the world of virtual reality. This event is a perfect opportunity to explore new techniques and to connect with like-minded gamers. Coordination and teamwork are the key points to achieve the goal. Patience and time management are the main keys for winning.",
-      rules: [
-        "Number of participants is 03",
-        "Skills required - Basic gaming skills with planning, strategy, and multitasking.",
-        "Requirements - Each team must carry at least one laptop (with mouse, keyboard, charger, earphones) and mobile phones with them.",
-      ],
-      heads: [
-        {
-          name: "Nikhil Madali",
-          phno: 7760337984,
-          photo: "/Photos/nikhil.jpg",
-        },
-        {
-          name: "Aditya Reddy",
-          phno: 9535997978,
-          photo: "/Photos/aditya.jpg",
-        },
-      ],
-      faculty: [{
-        name: "Prof. Vaibhav Ambekar",
-        phno: 9742426470,
-        photo: "/Photos/vaibhav.jpg",
-      },]
+      name: "Prof. Karuna Deshpande",
+      phno: 8747015516,
+      photo: "/Photos/karuna.jpg",
+    },]
+  },
+  {
+    id: "cyber-security",
+    name: "Metacortex",
+    tagline: "Decode,Defend,Dominate",
+    logo: "./Images/winners.webp",
+    description:
+      "Metacortex is basically cyber event which describes about the theft done on the DATA USED. Cybersecurity is the practice of protecting critical systems and sensitive information from digital attacks. cybersecurity measures are designed to combat threats.",
+    rules: [
+      "Number of participants is 02.",
+      "Skills required - Basic Linux commands, services,basics knowledge about web.",
+      "Requirements - 1 Laptop.",
+    ],
+    heads: [
+      {
+        name: "Amar Badiger",
+        phno: 8861142545,
+        photo: "/Photos/amar.jpg",
+      },
+      {
+        name: "Tejasvi Bhadavankar",
+        phno: 7892288512,
+        photo: "/Photos/tejasvi.jpg",
+      },
+    ],
+    faculty: [{
+      name: "Prof. Govind Huligol",
+      phno: 9986296434,
+      photo: "/Photos/govind.jpg",
+    },{
+      name: "Mrs.Pooja",
+      phno: 9986296434,
+      photo: "/Photos/pooja.jpg",
+    },
+  ]
+  },
+  {
+    id: "data-analytics",
+    name: "Stellar-Insights",
+    tagline: "Charling The unexplored Dimension",
+    logo: "./Images/winners.webp",
+    description:
+      "Dive into the world of DATA ANALYTICS, where data meets innovation, numbers tell stories, and turning data into game changing insights. Here every row counts, every column matters. Get ready to harness the power of data and step into the realm of Data Analytics.",
+    rules: [
+      "Number of participants is 02.",
+      "Skills required - Oracle, Power BI, Tableau, Excel, SPSS.",
+      "Requirements - 1 Laptop, Pendrive.",
+    ],
+    heads: [
+      {
+        name: "Mandar Kanbargi",
+        phno: 8746970092,
+        photo: "/Photos/mandar.jpg",
+      },
+      {
+        name: "Sakshi Patankar",
+        phno: 7760882407,
+        photo: "/Photos/sakshi.jpg",
+      },
+    ],
+    faculty: [{
+      name: "Prof. Shruti Kulkarni",
+      phno: 8792242249,
+      photo: "/Photos/shruti.jpg",
     },
     {
-      id: "happy-hours",
-      name: "Melody Munch",
-      tagline: "Tasty tune & Game delights.",
-      logo: "./Images/winners.webp",
-      description:
-        "Embark on a delightful journey of flavors, laughter, and melody at our upcoming College Extravaganza! Join us for a spectacular Food Fest where culinary delights from around the world will tantalize your taste buds. Engage in fun-filled games that promise laughter and excitement. And let the stage come alive with our Singing Event, showcasing the incredible vocal talents within our college community.",
-      rules: [
-        "Each team should have their food stall compulsorily.",
-        "Use of plastic items are banned, instead use paper cups or paper plates if required.",
-        "Team members should prepare chart boards or tags related to their stall. Marks will be given based on creativity of the stall.",
-        "A team of 5 participants that includes 1 volunteer for keeping account of money.",
-        "Only pure vegetarian food items are allowed.",
-        "Food stalls are limited to flameless cooking only.",
-        "Each team should get the menu approved  from the event mentor on 5th January.",
-        "If the team does not earn any profit then no reimbursement of the money will be made by college.",
-        "Profit earned will be given to charity.",
-      ],
-      heads: [
-        {
-          name: "Sharadh Dongare",
-          phno: 9902275157,
-          photo: "/Photos/dongare.jpg",
-        },
-        {
-          name: "Khushi Jituri",
-          phno: 9986535605,
-          photo: "/Photos/khushi.jpg",
-        },
-      ],
-      faculty:[{
-        name: "Prof. Vaishali Kale",
-        phno: 7846058520,
-        photo: "/Photos/vaishali.jpg",
-      },]
+      name: "Prof. Jyoti Patil",
+      phno: 8884988033,
+      photo: "/Photos/jyoti.jpeg",
+    },{
+      name:"Mrs. Shilpa",
+      phno: 8884988033,
+      photo: "/Photos/Shilpa.jpeg",
+    }]
+  },
+  {
+    id: "gaming",
+    name: "Ojingo",
+    tagline: "Dare To Play,Dare To Win,Dare To Survive.",
+    logo: "./Images/winners.webp",
+    description:
+      "Upgrade your reflexes and explore the world of virtual reality, this event is a perfect opportunity to explore new techniques and to connect with the like minded gamer. Coordination and team work are the key points to achieve the goal. Patience and time management is the main key for winning.",
+    rules: [
+      "Number of participants is 01",
+      "Skills required - Basic gaming skills with planning, strategy,multitasking and basic coding skills.",
+      "Requirements - Each team must carry at least one laptop (with mouse, keyboard, charger, earphones) and mobile phones with them.",
+    ],
+    heads: [
+      {
+        name: "Nikhil Madali",
+        phno: 7760337984,
+        photo: "/Photos/nikhil.jpg",
+      },
+      {
+        name: "Aditya Reddy",
+        phno: 9535997978,
+        photo: "/Photos/aditya.jpg",
+      },
+    ],
+    faculty: [{
+      name: "Prof. Abhishek",
+      phno: 9742426470,
+      photo: "/Photos/abhishek.jpg",
+    },]
+  },
+  {
+    id: "designing",
+    name: "Artistic Heist",
+    tagline: "Craft Create Conquer.",
+    logo: "./Images/winners.webp",
+    description:
+      "Unleash your creativity and join us for a designing event that will inspire and challenge you.This event is the perfect opportunity to explore new techniques to connect with like-minded individuals. Don't miss this chance to push your boundaries and take your skills to the next level.",
+    rules: [
+      "Number of participants is 02",
+      "Skills required - Basic graphic designing knowledge and time management skills arts.",
+      "Requirements - 2 Laptop, Adobe illustrator, Photoshop,premiere pro,After effects, Krita, Adobe XD ,Figma ,Blender and basics of HTML and CSS.",
+    ],
+    heads: [
+      {
+        name: "Prathamesh Patil",
+        phno: 9902275157,
+        photo: "/Photos/prathamesh.jpg",
+      },
+      {
+        name: "Samarth mutagekar",
+        phno: 9986535605,
+        photo: "/Photos/samarth.jpg",
+      },
+    ],
+    faculty:[{
+      name: "Prof. Rucha Bapat",
+      phno: 7846058520,
+      photo: "/Photos/rucha.jpg",
+    },{
+      name: "Ms Sneha",
+      phno: 7846058520,
+      photo: "/Photos/sneha.jpg",
     },
-  ];
-  
-  export default eventsData;
-  
+  ]
+  },
+];
+
+export default eventsData;
