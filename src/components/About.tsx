2634e5f5d093639d6efb8c8dd7941ae6753b1572
Priyankar_010_ <109033813+PriyankarCode010@users.noreply.github.com>
2024-05-29 18:12:10 +0530

    1st

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -6,23 +6,25 @@ type Props = {
   logo: string;
 };
 
-const About: React.FC<Props> = ({ description, rules, logo }) => {
-  console.log(logo);
+const About = (props: Props) => {
+
+  console.log("------", props.logo);
+
   return (
     <div className='flex w-full flex-col p-8'>
       <div className="flex w-full flex-col rounded-lg my-5 shadow-lg md:flex-row border-2 border-slate-950 bg-black text-white">
-        <div className="h-full w-full md:w-[55vw] flex-1 flex justify-center items-center border-b-2 border-slate-900 md:border-r-2 md:border-b-0">
+        <div className="h-full w-full md:w-[55vw] flex-1">
           <img
-            src={logo}
-            alt="Event logo"
-            className="h-80 w-80 rounded-md object-cover md:w-96 md:h-96"
+            src={props.logo}
+            alt="Description Logo"
+            className="h-full w-full rounded-md object-cover"
           />
         </div>
         <div className='flex-[2]'>
           <div className="p-4 flex flex-col gap-5">
-            <h2 className='uppercase text-4xl font-semibold text-slate-300 py-5'>About</h2>
-            <p className="text-2xl text-slate-500 break-words">
-              {description}
+            <h2 className='uppercase text-5xl font-semibold text-slate-300 py-5'>About</h2>
+            <p className="text-3xl text-slate-500 break-words">
+              {props.description}
             </p>
           </div>
         </div>
@@ -31,7 +33,7 @@ const About: React.FC<Props> = ({ description, rules, logo }) => {
         <h2 className='text-5xl md:text-8xl font-bold textShwd pl-0 md:pl-10'>Rules & Regulations</h2>
         <div className='shadow-xl px-3 md:px-32'>
           <ul className='list-disc md:text-2xl text-xl flex flex-col gap-5 w-[80vw] px-5 py-8'>
-            {rules.map((rule, index) => (
+            {props.rules.map((rule, index) => (
               <li key={index}>{rule}</li>
             ))}
           </ul>
