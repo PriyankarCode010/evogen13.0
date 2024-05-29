0607233dc5df49cb8ec80e181aa0e6c48ba5682b
Priyankar_010_ <109033813+PriyankarCode010@users.noreply.github.com>
2024-05-29 10:28:15 +0530

    1st

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -19,7 +19,7 @@ const Intro = (props: Props) => {
       </div>
     </div>
     <div className='flex-grow'>
-      <span className='text-xs md:text-xl'>Karnatak Law Society&apos;s</span>
+      <span className='text-xs md:text-xl'>The Karnatak Law Society&apos;s</span>
         <br />
       <span className='font-semibold text-xs md:text-2xl'>Gogte College of Commerce</span>
         <br />
