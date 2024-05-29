b7a15672c64c493f4b04f742af3d842b05e5bb27
Priyankar_010_ <109033813+PriyankarCode010@users.noreply.github.com>
2024-05-29 18:54:28 +0530

    1st

--- src/data/eventsData.js
+++ src/data/eventsData.js
@@ -44,7 +44,7 @@ const eventsData = [
     id: "communication",
     name: "Cerebro Voice",
     tagline: "Where communication meets mystery.",
-    logo: "./Images/winners.webp",
+    logo: "../Images/winners.webp",
     description:
     "Dive into 'Upside Voice' where communication becomes an adventure. Engage in themed challenges blending mystery and creativity, from decoding messages to navigating twists in speeches.",
     rules: [
@@ -79,7 +79,7 @@ const eventsData = [
     id: "technocrats",
     name: "The Extraordinary Academy",
     tagline: "Ordinary is Overrated.",
-    logo: "./Images/events/logo3_2.png",
+    logo: "../Images/events/logo3_2.png",
     description:
     "Embark on a journey of intellectual prowess and finesse where extraordinary people meet and unleash their pure potential with all skills required to be a master of all traits. Participants demonstrate their mastery across a spectrum of disciplines, from the cerebral realms of logic and problem-solving to the artistry of performance and expression many much more.",
     rules: [
@@ -118,7 +118,7 @@ const eventsData = [
     id: "cyber-security",
     name: "Metacortex",
     tagline: "Decode,Defend,Dominate",
-    logo: "./Images/winners.webp",
+    logo: "../Images/winners.webp",
     description:
       "Metacortex is basically cyber event which describes about the theft done on the DATA USED. Cybersecurity is the practice of protecting critical systems and sensitive information from digital attacks. cybersecurity measures are designed to combat threats.",
     rules: [
@@ -153,7 +153,7 @@ const eventsData = [
     id: "data-analytics",
     name: "Stellar-Insights",
     tagline: "Charling The unexplored Dimension",
-    logo: "./Images/events/data_2.png",
+    logo: "../Images/events/data_2.png",
     description:
       "Dive into the world of DATA ANALYTICS, where data meets innovation, numbers tell stories, and turning data into game changing insights. Here every row counts, every column matters. Get ready to harness the power of data and step into the realm of Data Analytics.",
     rules: [
@@ -192,7 +192,7 @@ const eventsData = [
     id: "gaming",
     name: "Ojingo",
     tagline: "Dare To Play,Dare To Win,Dare To Survive.",
-    logo: "./Images/events/game.png",
+    logo: "../Images/events/game.png",
     description:
       "Upgrade your reflexes and explore the world of virtual reality, this event is a perfect opportunity to explore new techniques and to connect with the like minded gamer. Coordination and team work are the key points to achieve the goal. Patience and time management is the main key for winning.",
     rules: [
@@ -222,7 +222,7 @@ const eventsData = [
     id: "designing",
     name: "Artistic Heist",
     tagline: "Craft Create Conquer.",
-    logo: "./Images/events/design.jpg",
+    logo: "../Images/events/design.jpg",
     description:
       "Unleash your creativity and join us for a designing event that will inspire and challenge you.This event is the perfect opportunity to explore new techniques to connect with like-minded individuals. Don't miss this chance to push your boundaries and take your skills to the next level.",
     rules: [
