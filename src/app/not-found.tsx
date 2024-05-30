b268c9549fb15b51fad429263c9c6c545640a2a9
Priyankar_010_ <109033813+PriyankarCode010@users.noreply.github.com>
2024-05-30 17:05:57 +0530

    1st

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -5,24 +5,30 @@ type Props = {}
 
 const NotFound = (props: Props) => {
   return (
-    <div className='relative'>
-      <div className="h-screen flex flex-col justify-center items-center bg-[url('/Images/first_404.jpg')] bg-cover bg-no-repeat">
-      <div className='flex flex-col justify-center items-center w-full h-screen translate-x-[-0%] bg-[#0009]'></div>
-        <div className='absolute h-screen w-full'>
-        <nav>
-          <img src="./Images/EVOGEN.png" alt="LOGO" 
-          width={200}
-          height={200}  
-          />
-        </nav>
-          <img src="./Images/four04.png" alt="logo" 
-          width={300}
-          height={300}
-        />
+      <div className='flex flex-col justify-center items-center w-full h-screen overflow-hidden'>
+        <div className=' h-screen w-full bg-black'>
+          <nav className='absolute w-full bg-black p-5 border-b-2'>
+            <img 
+            src="./Images/EVOGEN.png" 
+            alt="LOGO" 
+            className=''
+            width={200}
+            height={200}  
+            />
+          </nav>
+          <div className='h-full flex flex-col justify-center items-center'>
+            <div className='flex justify-between items-center text-white gap-0 md:gap-10'>
+              <img 
+              className='h-52 w-52 p-2 md:h-96 md:w-96'
+              src="./Images/four04.png" 
+              alt="logo" 
+              />
+              <h1 className='text-6xl md:text-8xl'>404</h1>
+            </div>
           <Link href="/"><div className='text-white bg-red-700 px-3 py-2 hover:bg-red-600'>take me home</div></Link>
+          </div>
         </div>
       </div>
-    </div>
   )
 }
 
