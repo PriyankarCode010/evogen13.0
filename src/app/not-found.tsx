8fee1d6394e87363fe2f85c960ed787fb371cf8e
Priyankar_010_ <109033813+PriyankarCode010@users.noreply.github.com>
2024-05-30 16:40:45 +0530

    1st

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -5,9 +5,23 @@ type Props = {}
 
 const NotFound = (props: Props) => {
   return (
-    <div className='h-screen flex flex-col justify-center items-center bg-black text-red-700'>
-      <h1>404</h1>
-      <Link href="/"><div className='text-white bg-red-700 px-3 py-2 hover:bg-red-600'>take me home :)</div></Link>
+    <div className='relative'>
+      <div className="h-screen flex flex-col justify-center items-center bg-[url('/Images/first_404.jpg')] bg-cover bg-no-repeat">
+      <div className='flex flex-col justify-center items-center w-full h-screen translate-x-[-0%] bg-[#0009]'></div>
+        <div className='absolute h-screen w-full'>
+        <nav>
+          <img src="./Images/EVOGEN.png" alt="LOGO" 
+          width={200}
+          height={200}  
+          />
+        </nav>
+          <img src="./Images/four04.png" alt="logo" 
+          width={300}
+          height={300}
+        />
+          <Link href="/"><div className='text-white bg-red-700 px-3 py-2 hover:bg-red-600'>take me home</div></Link>
+        </div>
+      </div>
     </div>
   )
 }
