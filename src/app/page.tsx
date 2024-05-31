12c3758690a323560b807fa8f47d03c3c5f1176f
Priyankar_010_ <109033813+PriyankarCode010@users.noreply.github.com>
2024-05-31 08:59:59 +0530

    1st

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
+"use client";
 import Footer from "@/components/Footer";
+import { useEffect } from 'react';
 import Intro from "@/components/Intro";
 import MainOrg from "@/components/MainOrg";
 import PhotoCard from "@/components/PhotoCard";
@@ -6,6 +8,11 @@ import Previouswinner from "@/components/Previouswinner";
 import WhatsNew from "@/components/WhatsNew";
 
 export default function Home() {
+
+  useEffect(() => {
+    window.scrollTo({ top: 10, behavior: 'smooth' });
+  }, []);
+
   return (
     <div className="select-none">
       <Intro/>
