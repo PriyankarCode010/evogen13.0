bd65372f6bf7844e53cb9a9be0035dbba4157fb9
Priyankar_010_ <109033813+PriyankarCode010@users.noreply.github.com>
2024-06-01 01:12:14 +0530

    1st

--- src/components/MainOrg.tsx
+++ src/components/MainOrg.tsx
@@ -6,11 +6,15 @@ import data from '@/data/main.js';
 type Props = {};
 
 const MainOrg = (props: Props) => {
-  const mainorg = data.find(event => event.id === "mainorg")
+  const main = data.find(event => event.id === "main")
   const eventorg = data.find(event => event.id === "eventorg")
   const studorg = data.find(event => event.id === "studorg");
   const dev = data.find(event => event.id === "dev");
   const [show, setShow] = useState(false);
+
+  if (!main || !dev) {
+    return <div>Data not found</div>;
+  }
 
   return (
     <div className={`text-white pt-10 flex flex-col justify-center items-center border-b-2 border-red-800 ${show ? "bg-white" : "bg-black"} `}>
@@ -20,7 +24,7 @@ const MainOrg = (props: Props) => {
             Organizing Committee
           </h2>
           <div className="flex flex-col md:flex-row md:justify-evenly justify-center items-center gap-10 w-full">
-            {mainorg.ppl.map((fac, index) => (
+            {main.ppl.map((fac, index) => (
               <MphotoCard key={index} name={fac.name} role={fac.role} photo={fac.photo} />
             ))}
           </div>
