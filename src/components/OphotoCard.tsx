d2163cc886b6f002f5850c905834abf153bc4628
Priyankar_010_ <109033813+PriyankarCode010@users.noreply.github.com>
2024-06-02 03:25:39 +0530

    1st

--- src/components/OphotoCard.tsx
+++ src/components/OphotoCard.tsx
@@ -21,11 +21,13 @@ const OphotoCard = (props: Props) => {
             className="z-0 h-full w-full rounded-md object-cover"
           />
           <div className="absolute inset-0 bg-gradient-to-t from-gray-900 to-transparent rounded-lg"></div>
-          <div className="absolute bottom-4 left-4 text-left">
-            <h1 className="text-lg font-semibold text-white">{props.name}</h1>
-            <p className="mt-2 text-sm text-gray-300">
-              {props.phno || ''}
-            </p>
+          <div className="absolute bottom-4 left-4 text-left w-full">
+            <div>
+              <h1 className="text-lg font-semibold text-white">{props.name}</h1>
+              <p className="mt-2 text-sm text-gray-300">
+                {props.phno || ''}
+              </p>
+            </div>           
           </div>
         </div>
       </Fade>
