580db9a0a9c2b2f111b999065c2b51d4f664091b
Priyankar_010_ <109033813+PriyankarCode010@users.noreply.github.com>
2024-06-03 00:13:50 +0530

    1st

--- src/components/EventPage.tsx
+++ src/components/EventPage.tsx
@@ -59,7 +59,7 @@ const EventPage = ({ id }: Props) => {
           </div>
         </div>
         <hr />
-        <div className="flex justify-center p-3 gap-2">
+        <div className="flex flex-col justify-center p-3 gap-2">
           <About rules={rules} description={description} logo={logo} />
           <div className="px-5 py-2 rounded-md bg-red-600 text-white text-2xl hover:bg-red-500">Register</div>
         </div>
