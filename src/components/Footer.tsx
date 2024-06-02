28273acef8672e89b9a2c91e0b68f1e6edfd0cd6
Priyankar_010_ <109033813+PriyankarCode010@users.noreply.github.com>
2024-06-02 23:54:30 +0530

    1st

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Link from 'next/link';
 import { LuMountain } from "react-icons/lu";
+import { FaInstagram, FaSquareFacebook } from "react-icons/fa6";
 
 
 type Props = {}
@@ -10,21 +11,11 @@ const Footer = (props: Props) => {
     <footer className="bg-black py-6 px-4 md:px-6 flex items-center justify-between">
         <div className="flex items-center gap-2">
           <LuMountain  className="h-6 w-6 text-red-600" />
-          <p className="text-sm text-red-600 ">© 2024 Acme Inc. All rights reserved.</p>
+          <p>&copy; 2024 Evogen BCA. All Rights Reserved.</p>
         </div>
         <nav className="flex items-center gap-4">
-          <Link
-            className="text-sm text-red-600 hover:text-red-500"
-            href="#"
-          >
-            Terms
-          </Link>
-          <Link
-            className="text-sm text-red-600 hover:text-red-500"
-            href="#"
-          >
-            Privacy
-          </Link>
+          <FaInstagram />
+          <FaSquareFacebook />
         </nav>
       </footer>
   )
