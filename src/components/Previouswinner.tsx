5d05c9674bff0af4042b05670ee34eaae5becd92
Priyankar_010_ <109033813+PriyankarCode010@users.noreply.github.com>
2024-06-02 21:22:41 +0530

    1st

--- src/components/Previouswinner.tsx
+++ src/components/Previouswinner.tsx
@@ -1,8 +1,6 @@
-import React from 'react'
+import React from 'react';
 
-type Props = {}
-
-const Previouswinner = (props: Props) => {
+const Previouswinner = () => {
   return (
     <div className='bg-black'>
       <div className='py-16 px-4 md:py-32 md:px-20 lg:px-40 flex flex-col gap-16 md:gap-32'>
@@ -19,13 +17,13 @@ const Previouswinner = (props: Props) => {
           <div className='flex-1 text-center md:text-left'>
             <h1 className='text-red-600 text-3xl md:text-4xl lg:text-6xl font-extrabold mb-4 md:mb-8'>Evogen 12.0</h1>
             <div className='text-white text-sm md:text-base lg:text-lg'>
-            Last year's fest, was a resounding success, filled with vibrant energy and diverse activities. The festivity featured a wide array of events. The winners were recognized for their outstanding talents and hard work, leaving a lasting impression on everyone present. The fest not only provided a platform for showcasing skills but also fostered camaraderie and community spirit. We look back on last year's fest with pride and look forward to continuing this tradition of excellence.
+              Last year's fest, was a resounding success, filled with vibrant energy and diverse activities. The festivity featured a wide array of events. The winners were recognized for their outstanding talents and hard work, leaving a lasting impression on everyone present. The fest not only provided a platform for showcasing skills but also fostered camaraderie and community spirit. We look back on last year's fest with pride and look forward to continuing this tradition of excellence.
             </div>
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Previouswinner
+export default Previouswinner;
