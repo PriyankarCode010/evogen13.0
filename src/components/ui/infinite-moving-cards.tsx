0133fb87408d67f3646e34d71dc3a98a77248b84
Priyankar_010_ <109033813+PriyankarCode010@users.noreply.github.com>
2024-06-03 16:42:52 +0530

    1st

--- src/components/ui/infinite-moving-cards.tsx
+++ src/components/ui/infinite-moving-cards.tsx
@@ -62,7 +62,7 @@ export const InfiniteMovingCards = ({
   const getSpeed = () => {
     if (containerRef.current) {
       if (speed === "fast") {
-        containerRef.current.style.setProperty("--animation-duration", "10s");
+        containerRef.current.style.setProperty("--animation-duration", "15s");
       } else if (speed === "normal") {
         containerRef.current.style.setProperty("--animation-duration", "20s");
       } else {
