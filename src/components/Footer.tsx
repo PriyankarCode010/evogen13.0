33a9128d5027767dc21df1752c9f431cf4707781
Priyankar_010_ <109033813+PriyankarCode010@users.noreply.github.com>
2024-06-04 20:27:06 +0530

    1st

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,14 +7,14 @@ type Props = {}
 
 const Footer = (props: Props) => {
   return (
-    <footer className="relative bg-black py-6 px-4 md:px-6 flex items-center justify-between text-red-600">
+    <footer className=" bg-black py-6 px-4 md:px-6 flex items-center justify-between text-red-600">
         <div className="flex items-center gap-2">
           <LuMountain  className="h-6 w-6" />
           <p className="text-sm md:text-base">&copy; 2024 Evogen BCA. All Rights Reserved.</p>
         </div>
-        <nav className="flex items-center gap-4">
+        <nav className=" relative flex items-center gap-4">
         <div 
-         className="absolute bottom-4 right-4 cursor-pointer" 
+         className="absolute bottom-6 right-4 cursor-pointer" 
          onClick={() => window.open("https://www.instagram.com/gcc_bca", '_blank', 'noopener,noreferrer')}
         ><FaInstagram className="text-xl" /></div>
         {/* <div onClick={() => window.open("https://www.instagram.com/gcc_bca", '_blank', 'noopener,noreferrer')}><FaSquareFacebook className="text-lg" /></div> */}
